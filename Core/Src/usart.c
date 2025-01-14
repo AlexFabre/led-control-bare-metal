db257c25cbb4033de6cc4070b9289b351958c49f
Alex Fabre <alex.fabre@rtone.fr>
2025-01-14 15:38:19 +0100

    fix UART2 to non-blocking

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -123,12 +123,24 @@ void uart2_print(const char *msg, const size_t len)
 
     infoPrintln("UART2 Tx started");
 
-    int err = HAL_UART_Transmit(&huart2, (uint8_t *)msg, (uint16_t)len, 10 * len);
+    int err = HAL_UART_Transmit_IT(&huart2, (uint8_t *)msg, (uint16_t)len);
     if (err != HAL_OK) {
         errorPrintln("HAL Tx error (%d)", err);
     }
+}
 
-    infoPrintln("UART2 Tx complete");
+/**
+ * @brief Tx Transfer completed callback.
+ * @param huart UART handle.
+ * @retval None
+ */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart == &huart2) {
+        infoPrintln("UART2 Tx complete");
+    } else {
+        errorPrintln("Unknown Tx complete IRQ");
+    }
 }
 
 /* USER CODE END 1 */
